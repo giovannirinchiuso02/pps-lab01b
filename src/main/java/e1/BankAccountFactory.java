702df50f4afa218c55aae4d513fb725defeea2ce
Giovanni Rinchiuso <giovanni.rinchiuso@studio.unibo.it>
2025-03-03 13:16:34 +0100

    Implemented Decorator pattern for GoldBankAccount

--- src/main/java/e1/BankAccountFactory.java
+++ src/main/java/e1/BankAccountFactory.java
@@ -2,11 +2,19 @@ package e1;
 
 public class BankAccountFactory {
     private static final int SILVER_FEE = 1;
+    private static final int GOLD_FEE = 0;
     private static final int SILVER_MINOR_AMOUNT = 0;
+    private static final int GOLD_MINOR_AMOUNT = 500;
 
     public BankAccount createSilverBankAccount() {
         return new DecoratorBankAccount(new FeeDecorator(new CanWithdrawDecorator(new CoreBankAccount(), SILVER_MINOR_AMOUNT), SILVER_FEE));
     }
+
+    public BankAccount createGoldBankAccount() {
+        return new DecoratorBankAccount(new FeeDecorator(new CanWithdrawDecorator(new CoreBankAccount(), GOLD_MINOR_AMOUNT), GOLD_FEE));
+    }
+
+
 
 
 
--- src/test/java/e1/BankAccountTest.java
+++ src/test/java/e1/BankAccountTest.java
@@ -11,36 +11,55 @@ public class BankAccountTest {
     private BankAccountFactory bankAccountFactory;
     private BankAccount accountSilver;
     private BankAccount accountGold;
+    private static final int INITIAL_DEPOSIT = 1000;
+    private static final int INITIAL_WITHDRAWAL = 200;
+    private static final int WRONG_SILVER_WITHDRAWAL = 1200;
 
     @BeforeEach
     void init(){
         this.bankAccountFactory = new BankAccountFactory();
         this.accountSilver = this.bankAccountFactory.createSilverBankAccount();
-        this.accountGold = new GoldBankAccount();
+        this.accountGold = this.bankAccountFactory.createGoldBankAccount();
     }
 
     @Test
     public void testInitiallyEmpty() {
         assertEquals(0, this.accountSilver.getBalance());
+        assertEquals(0, this.accountGold.getBalance());
     }
 
     @Test
     public void testCanDeposit() {
-        this.accountSilver.deposit(1000);
-        assertEquals(1000, this.accountSilver.getBalance());
+        this.accountSilver.deposit(INITIAL_DEPOSIT);
+        this.accountGold.deposit(INITIAL_DEPOSIT);
+        assertEquals(INITIAL_DEPOSIT, this.accountSilver.getBalance());
+        assertEquals(INITIAL_DEPOSIT, this.accountGold.getBalance());
     }
 
     @Test
     public void testCanWithdraw() {
-        this.accountSilver.deposit(1000);
-        this.accountSilver.withdraw(200);
+        this.accountSilver.deposit(INITIAL_DEPOSIT);
+        this.accountGold.deposit(INITIAL_DEPOSIT);
+        this.accountSilver.withdraw(INITIAL_WITHDRAWAL);
+        this.accountGold.withdraw(INITIAL_WITHDRAWAL);
         assertEquals(799, this.accountSilver.getBalance());
+        assertEquals(800, this.accountGold.getBalance());
     }
 
     @Test
     public void testCannotWithdrawMoreThanAvailable(){
-        this.accountSilver.deposit(1000);
+        this.accountSilver.deposit(INITIAL_DEPOSIT);
+        this.accountGold.deposit(INITIAL_DEPOSIT);
         assertThrows(IllegalStateException.class, () -> this.accountSilver.withdraw(1200));
+        assertThrows(IllegalStateException.class, () -> this.accountGold.withdraw(1600));
+    }
+
+    @Test
+    public void testCanDepositMoreThanAvailable(){
+        this.accountGold.deposit(INITIAL_DEPOSIT);
+        this.accountGold.withdraw(1200);
+        assertEquals(-200, this.accountGold.getBalance());
+
     }
 
 }
